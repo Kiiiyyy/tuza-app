80b35201352cd081ecb0c2a5b55105e4cd5562a9
Kiiiyyy <dzakyramadhani14510@gmail.com>
2025-10-08 10:28:32 +0700

    tambah api

--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,9 @@
 <?php
+use Illuminate\Http\Request;
 
 Route::post('auth/register', \App\Http\Controllers\Api\Auth\RegisterController::class);
+Route::post('auth/login', \App\Http\Controllers\Api\Auth\LoginController::class);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
